7f10072cb7fcd7cfffac8f0566f859c193cadb33
ShoyaAlm <shoyaalimi@gmail.com>
2023-04-22 15:04:11 +0330

    Uploaded the 'Objective' database to the mongoDB

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./DB/connectDB')
+
 require('dotenv').config()
 
 const router = require('./routes/routes')
 
 app.use(express.static('./static'))
-// app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false}))
 
 app.use('/api/v1', router)
 app.use(express.json())
 
-const port = 3000;
 
 
-app.listen(port, (req, res) => {
-    console.log(`Server on port ${port}`);
-})
+const port = process.env.port || 8080;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, `Server on port ${port}...`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
--- model/objective.js
+++ model/objective.js
@@ -8,7 +8,7 @@ const Objective = new mongoose.Schema({
         maxLength: [120, 'Name is too long']
     },
     date:{
-        type: date,
+        type: Date,
         required: [true, 'Please provide your target date of the objective']
     },
     completed:{

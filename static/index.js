4e588ffa1d0c6580230820583f2d229dd32428af
ShoyaAlm <shoyaalimi@gmail.com>
2023-05-07 16:10:07 +0330

    trying to sync backend with front

--- static/index.js
+++ static/index.js
@@ -1,35 +1,61 @@
-
-const objectiveIDDOM = document.querySelector('.objective-id')
-const objectiveNameDOM = document.querySelector('.objective-name')
-const objectiveDateDOM = document.querySelector(".objective-date")
-const objectiveCompletedDOM = document.querySelector(".objective-completed")
-
+// document.addEventListener('DOMContentLoaded', () => {
+// })
 
 const nameInputDOM = document.querySelector('.name-input')
 const dateInputDOM = document.querySelector('.date-input')
 const completedInputDOM = document.querySelector('.completed-input')
 const formDOM = document.querySelector(".objective-form")
 
-
+console.log(formDOM);
 
 formDOM.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const objectiveName = nameInputDOM.value
-        const objectiveDate = dateInputDOM.value
-        const objectiveCompleted = completedInputDOM.value
+  e.preventDefault();
+  const objectiveName = nameInputDOM.value
+  const objectiveDate = dateInputDOM.value
+  const objectiveCompleted = completedInputDOM.value
 
-        const newObjective = {objectiveName, objectiveDate, objectiveCompleted}
-        console.log(newObjective);
-        
-        try {
+  const newObjective = {objectiveName, objectiveDate, objectiveCompleted}
+  console.log(newObjective);
 
-            await axios.post('/api/v1', { newObjective })
-            // nameInputDOM.value = ''
-            // dateInputDOM.value = ''
-            // completedInputDOM.value = ''
-        
-        } catch (error) {
-            console.log(error.response.data);
-        }
+  try {
+    const result = await axios.post('/api/v1', { newObjective })
+    console.log(result);
+    nameInputDOM.value = ''
+    dateInputDOM.value = ''
+    completedInputDOM.value = ''
 
-    })
+  } catch (error) {
+    console.log(error);
+  }
+
+})
+      
+      
+      // const result = await axios({
+      //     data: {newObjective},
+      //     method: 'POST',
+      //     url: `/api/v1`, 
+      //     headers: {
+      //       'Authorization': '',
+      //       'Content-Type': '',
+      //     },
+      //   });
+      //   let data = result.data
+      //   console.log(data);
+      
+      // nameInputDOM.value = ''
+      // dateInputDOM.value = ''
+      // completedInputDOM.value = ''
+
+
+    // this GET method works pretty well
+
+    // axios.get('/api/v1')
+    //             .then((getResponse) => {
+    //                 console.log("GET Response")
+    //                 console.log(getResponse.data);
+    //                 data = getResponse.data;
+    //               })
+    //               .catch(function (error) {
+    //                 console.log(error);
+    //               });
